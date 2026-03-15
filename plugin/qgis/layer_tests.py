46bfa4b81171a6fa3227fa3e353ac04133d1d3c2
Eron Lloyd <eronlloyd@protonmail.com>
2026-03-15 17:00:10 -0400

    Moving plugin code out of repo

--- plugin/qgis/layer_tests.py
+++
@@ -1,5 +0,0 @@
-project = QgsProject.instance()
-entities_layer = project.mapLayersByName('entities')[0]
-print(entities_layer.attributeList())
-print(entities_layer.attributeDisplayName(0))
-print([f for f in entities_layer.fields()])
